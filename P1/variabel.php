f1e3e23818ffeebba4cd4aa2a4b9363d300625dd
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2021-09-29 11:12:28 +0700

    P1

---
+++ P1/variabel.php
@@ -0,0 +1,25 @@
+<?php
+// Variabel
+
+$panjang = "12";
+$lebar = 18;
+// Kondisi/Express
+$tinggi = ($panjang > 20) ? 100 : 0;
+
+// Sample penjabaran
+// Percabangan if else
+/*
+$tinggi = 100;
+if ($panjang > 20) {
+    echo "100";
+} else {
+    echo "0";
+}
+*/
+
+echo "Panjang = $panjang <br>";
+echo "Lebar = $lebar <br>";
+echo "Tinggi = $tinggi";
+
+// $hasil = $panjang + $lebar;
+// echo ("Hasil = $hasil");
